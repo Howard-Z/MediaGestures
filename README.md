766efd35d2c9e9659e2367a3bb331d9e1eb0816f
JacobR25 <jacobdryan1@gmail.com>
2024-01-29 01:13:01 -0800

    Added some notes
    Torch comin soon

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MediaGestures
 UCLA CS 188 Final Project
+
+There's cuda versions of pytorch, if you need that use the command: 
+
+`pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121` 
--- parse_hand_landmarker.py
+++ parse_hand_landmarker.py
@@ -1,11 +1,13 @@
 def parser(handLandmarker):
-    if handLandmarker.hand_landmarks == []:
+    if handLandmarker.hand_landmarks == []: #makes sure it's not empty
         return None
+    
     llandmarks = handLandmarker.hand_landmarks[0]
-    llist = [coord for landmark in llandmarks for coord in [landmark.x, landmark.y, landmark.z]]
-    if len(handLandmarker.hand_landmarks) == 2:
-        rlandmarks = handLandmarker.hand_landmarks[1]
+    llist = [coord for landmark in llandmarks for coord in [landmark.x, landmark.y, landmark.z]] #left hand list
+    
+    if len(handLandmarker.hand_landmarks) == 2: #checks right hand list exists
+        rlandmarks = handLandmarker.hand_landmarks[1]   #same process
         rlist = [coord for landmark in rlandmarks for coord in [landmark.x, landmark.y, landmark.z]]
     else:
-        rlist = None
-    return llist,rlist
+        rlist = None #else make sure it's none
+    return llist, rlist #return both as tuple
