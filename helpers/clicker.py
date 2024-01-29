0726a824c2528ae4ed280c83531b9dcc8d8ea69d
Aidan Jan <ajan890@g.ucla.edu>
2024-01-29 00:23:52 -0800

    this was supposed to be in the previous commit but i forgor :skull:

---
+++ helpers/clicker.py
@@ -0,0 +1,35 @@
+import pyautogui
+from time import sleep
+import argparse
+
+def click_n_times(n: int):
+    for i in range(1, n + 1):
+        if i < n + 1:
+            sleep(2)
+            print(f"Beginning next recording in 3 for video {i}...", end='\r')
+            sleep(1)
+            print(f"Beginning next recording in 2 for video {i}...", end='\r')
+            sleep(1)
+            print(f"Beginning next recording in 1 for video {i}...", end='\r')
+            sleep(1)
+            print("                                               ", end='\r')
+
+        print(f"Recording video {i}")
+        pyautogui.click()
+        sleep(4)
+        pyautogui.click()
+        print(f"Video {i} completed.")
+    print("Done recording.")
+    
+    
+def main():    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num_clicks', type=int, help="number of times to click")
+    
+    args = parser.parse_args()
+
+    num_clicks = args.num_clicks
+    click_n_times(num_clicks)
+    
+if __name__ == '__main__':
+    main()
